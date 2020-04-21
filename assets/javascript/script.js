6fe7679d962aba73d17734ac00ab88b307b6a901
Isha Bali <ishabali@gmail.com>
2020-04-21 04:16:42 -0500

    Check added that at least one type of character-set has to be there in the password

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -43,9 +43,9 @@ function makeFinalPassArray(fullPassArray2,userLen){
   for (i = 0; i < userLen; i++){
     // Generate random no. between 0 and len1
     randNo = Math.floor(Math.random()*len1);
-    alert("random no." + randNo);
+   // alert("random no." + randNo);
     finalPassArray.push(fullPassArray2[randNo]);
-    alert(finalPassArray[i]);
+   // alert(finalPassArray[i]);
   }
   return finalPassArray;
 }
@@ -57,32 +57,52 @@ function makePasswordArray(){
 
 
   //var a = 1;
+  // var check1 = false;
+  // check1 = document.getElementById("check1").checked;
+  // var check2 = false;
+  // check2 = document.getElementById("check2").checked;
+  // var check3 = false;
+  // check3 = document.getElementById("check3").checked;
+  // var check4 = false;
+  // check4 = document.getElementById("check4").checked;
+
+
+  // if ((check1 === false)&&(check2 === false)&&(check3 === false)&&(check4 === false)) {
+  //   alert("At Least Choose One Type of Characters for your Password");
+  //  // passChoices=[];
+  //   return passChoices;
+  // } 
+
+
   var check1 = false;
   check1 = document.getElementById("check1").checked;
   var lCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   // if (a===1){
   if (check1===true) {
     // alert("in if a = 1");
+    var lCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     passChoices = passChoices.concat(lCase);
   }
 
   //var a = 1;
   var check2 = false;
   check2 = document.getElementById("check2").checked;
-  var uCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  //var uCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   //if (a===1){
   if (check2===true) {
     // alert("in if a = 1");
+    var uCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     passChoices = passChoices.concat(uCase);
   }
 
   //var a = 1;
   var check3 = false;
   check3 = document.getElementById("check3").checked;
-  var nPass = ['0','1','2','3','4','5','6','7','8','9'];
+  //var nPass = ['0','1','2','3','4','5','6','7','8','9'];
 //  if (a===1){
   if (check3===true) {
 //    alert("a" + a);
+    var nPass = ['0','1','2','3','4','5','6','7','8','9'];
     passChoices = passChoices.concat(nPass);
   }
 
@@ -91,10 +111,12 @@ function makePasswordArray(){
   check4 = document.getElementById("check4").checked;
   //alert("check4"+check4);
   //var spCase = ['','!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',',',':',';','<','=','>','?','@','[','\',']','^','_','`','{','|','}','~'];*/
-  var spCase = ['','!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',',',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+  //var spCase = ['','!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',',',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
  //if (a===1){
-    if (check4===true) {
-    passChoices = passChoices.concat(spCase);
+  if (check4===true) {
+  //  var spCase = ['','!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',',',':',';','<','=','>','?','@','[','\',']','^','_','`','{','|','}','~'];*/
+      var spCase = ['','!','"','#','$','%','&',"'",'(',')','*','+','-','.','/',',',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
+      passChoices = passChoices.concat(spCase);
   }
 
 // This for loop has to be removed later
@@ -139,11 +161,35 @@ function writePassword(){
  /*   alert("in write password function"); */
     document.getElementById("password").innerHTML = "";
     var userChoices=[];
-    alert("length1");
+ //   alert("length1");
  //   userChoices=getUserChoices();
-    fullPassArray=[];
+   
+
+    var check1 = false;
+    check1 = document.getElementById("check1").checked;
+    var check2 = false;
+    check2 = document.getElementById("check2").checked;
+    var check3 = false;
+    check3 = document.getElementById("check3").checked;
+    var check4 = false;
+    check4 = document.getElementById("check4").checked;
+    if ((check1 === false)&&(check2 === false)&&(check3 === false)&&(check4 === false)) {
+      alert("At Least Choose One Type of Characters for your Password");
+      var finalPassword="";
+      document.getElementById("password").innerHTML = finalPassword;
+      return;
+    } 
+
+    var fullPassArray=[];
     fullPassArray=makePasswordArray();
-    alert("fullPassArrayLength"+fullPassArray.length);
+    // if (fullPassArray.length===0){
+    //   finalPassword="";
+    //   alert("At Least Choose One Type of Characters for your Password");
+    //   document.getElementById("password").innerHTML = finalPassword;
+    //   return;
+    // }
+
+   // alert("fullPassArrayLength"+fullPassArray.length);
     // Default Password Length = 8;
     var userLength = 8;
  //   userLength = document.getElementById("len1").value;
@@ -161,12 +207,12 @@ function writePassword(){
   }
  
  
-    alert("userLengthFin"+userLength);
+ //   alert("userLengthFin"+userLength);
     finalPassArray=makeFinalPassArray(fullPassArray,userLength);
     finalPassword="";
     for (i = 0; i < finalPassArray.length; i++){
       finalPassword = finalPassword + finalPassArray[i];
     }
-    alert(finalPassword);
+   // alert(finalPassword);
     document.getElementById("password").innerHTML = finalPassword;
 }
